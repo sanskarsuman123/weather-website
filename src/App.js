552d2dc77837dbbf70e43c1cd86678577254fa83
sanskarsuman123 <104655438+sanskarsuman123@users.noreply.github.com>
2022-05-22 20:56:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './App.css';
+import Wheatherapp from './components/wheatherapp';
+
+function App() {
+  return (
+    <Wheatherapp />
+  );
+}
+
+export default App;
